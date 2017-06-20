e621b4bac090aacdb42be090d0a7929436061594
Brian Schneider <schneid.brian@gmail.com>
2017-06-19 22:48:00 -0500

    Add portal page documentation

---
+++ _site/assets/js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+
+  //scroll fixed header
+  $(window).on("scroll", function(e) {
+    if (this.scrollY > 15) {
+      $('body').addClass("fixed");
+    } else {
+      $('body').removeClass("fixed");
+    }
+
+    if (this.scrollY > 35) {
+      $('.snow-header-nav').addClass('gradient');
+    } else {
+      $('.snow-header-nav').removeClass('gradient');
+    }
+  });
+});
