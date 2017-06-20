5bd716363d4ac458ab684aa5fc5db538e4da3da0
Brian Schneider <schneid.brian@gmail.com>
2017-06-19 22:50:33 -0500

    remove scroll code

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   $(".button-collapse").sideNav();
 
   //scroll fixed header
-  $(window).on("scroll", function(e) {
-    if (this.scrollY > 15) {
-      $('body').addClass("fixed");
-    } else {
-      $('body').removeClass("fixed");
-    }
+  // $(window).on("scroll", function(e) {
+  //   if (this.scrollY > 15) {
+  //     $('body').addClass("fixed");
+  //   } else {
+  //     $('body').removeClass("fixed");
+  //   }
 
-    if (this.scrollY > 35) {
-      $('.snow-header-nav').addClass('gradient');
-    } else {
-      $('.snow-header-nav').removeClass('gradient');
-    }
-  });
+  //   if (this.scrollY > 35) {
+  //     $('.snow-header-nav').addClass('gradient');
+  //   } else {
+  //     $('.snow-header-nav').removeClass('gradient');
+  //   }
+  // });
 });
